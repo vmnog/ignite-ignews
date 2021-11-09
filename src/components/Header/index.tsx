eed6b77ceb9f497782e1e44ffd5e73898c02b41d
Victor Nogueira <vmnogueira.ti@gmail.com>
2021-11-08 23:46:39 -0300

    chore: creating header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/assets/images/logo.svg" alt="ig.news" />
+        <nav className={styles.active}>
+          <a href="">Home</a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
